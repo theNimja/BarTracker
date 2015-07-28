aef8d8b5fe06fce9933fd3ab14209d88ffd24949
nimrod libman <nimrodlibman@yahoo.c.uk>
2015-07-28 10:24:25 +0100

    Bugs galore, but this needs to be pushed, so we can both work on them

--- reader.py
+++ reader.py
@@ -2,13 +2,29 @@ import os,socket,time
 ip = socket.gethostbyname(socket.gethostname())
 #killReadOrCreate , 0= read, 1= kill,2= create
 # can go to 2147483647
+
+
+
+
 def sendMessageToServer(productID,killReadOrCreate=0, corporateMessage= "Access from user"):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect((ip,8888))
     userName = socket.gethostname()+"@"+ip
-    
     msg = str(killReadOrCreate)+","+userName+","+str(productID)+","+corporateMessage
     print(msg)
     server.sendall(bytes(msg,"UTF-8"))
 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((ip,8888))
 sendMessageToServer("123",0)
+
+
+numLines= server.recv(1024)
+numLines=int(numLines)
+print(str(numLines))
+
+buffer=[]
+for i in range(numlines):
+    reply= server.recv(1024)
+    buffer.append(string(reply,"UTF-8"))
+print(buffer)
+input()
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import os,socket,datetime,sys
-HOST = ''   # Symbolic name, meaning all available interfaces
+
+
+
+HOST = ""   # Symbolic name, meaning all available interfaces
 PORTNUM = 8888 # Arbitrary non-privileged port
 port = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -16,7 +19,7 @@ print ('Socket now listening')
 #now keep talking with the client
 while True:
     while True:
-        #wait to accept a connection - blocking call
+        #wait to accept a connection
         conn, addr = port.accept()
         print ('Connected with ' + addr[0] + ':' + str(addr[1]))
         message=conn.recv(1024)
@@ -25,11 +28,23 @@ while True:
     message= message [2:len(message)-1]
     splitted= message.split(",")
     data={}
+    
     if (splitted[0]=="0"):
         data = {"productID":splitted[2],"accessFrom":splitted[1], "message":splitted[3]}
         print("Access to product "+data["productID"]+"  from "+data["accessFrom"]+", message is "+data["message"])
-        fobj = open("data/"+data["productID"]+".txt", "w")
-        
-        stringToWrite=datetime.datetime.now()+" "+data["accessFrom"]+" "+data["message"].replace(" ","_")
-        fobj.write(stringToWrite)
+
+        fobj = open("data/"+data["productID"]+".txt", "a")
+        stringToWrite=str(datetime.datetime.now())+" "+data["accessFrom"]+" "+data["message"].replace(" ","_")
+        fobj.write(stringToWrite+"\n")
         fobj.close()
+        fbin = open("data/"+data["productID"]+".txt","r")
+
+        mulligans= fbin.readlines()
+        fbin.close()
+        print(str(mulligans))
+        print(str(len(mulligans)))
+        conn.sendall(bytes(str(len(mulligans))))
+        for i in mulligans:
+            print("sending "+i)
+            conn.send(bytes(i,"UTF-8"))
+        fbin.close()
